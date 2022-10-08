bb3095d704b3b25c0186f38d8b9fa9cff0942a54
Muhammad Yasser <roktmy@gmail.com>
2022-10-08 18:42:22 +0200

    formatted src files

--- services/get_ip_data.go
+++ services/get_ip_data.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 )
 
-// const IP_URL = "https://ipapi.co/json/"
 const IP_URL = "https://ipapi.co/"
 
 type IPData struct {
@@ -40,28 +39,3 @@ func GetIPData(ip string) (*IPData, error) {
 	}
 	return ipData, nil
 }
-
-//
-//func GetIPData() (*IPData, error) {
-//	client := http.Client{}
-//	request, err := http.NewRequest("GET", IP_URL, nil)
-//	request.Header.Set("User-Agent", "ipapi.co/#go-v1.5")
-//
-//	response, err := client.Do(request)
-//	defer response.Body.Close()
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//	responseData, err := io.ReadAll(response.Body)
-//	if err != nil {
-//		return nil, err
-//	}
-//	ipData := new(IPData)
-//	err = json.Unmarshal(responseData, ipData)
-//	if err != nil {
-//		return nil, err
-//
-//	}
-//	return ipData, nil
-//}

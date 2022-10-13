f8dceff66b39a30df2c809a9c7954ede9f96c743
mdyssr <mhmmdyssraamr@gmail.com>
2022-10-14 00:22:48 +0200

    used prayers library to get data instead of our server getting it

--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module github.com/mdyssr/prayer-api
 
 go 1.19
 
+require github.com/labstack/echo/v4 v4.9.0
+
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/labstack/echo/v4 v4.9.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mdyssr/prayers v0.0.0-20221013221413-2b4d6122c190 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,23 +1,10 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
-	"github.com/mdyssr/prayer-api/models"
-	"github.com/mdyssr/prayer-api/services"
-	"github.com/mdyssr/prayer-api/utils"
+	"github.com/mdyssr/prayers"
+	"net/http"
 )
-
-const GetUserIPDataError = Error("Error getting user IP data")
-const GetPrayerMethodsError = Error("Error getting prayer methods")
-const GetPrayerDataError = Error("Error getting prayer data")
-
-type Error string
-
-func (e Error) Error() string {
-	return string(e)
-}
 
 func Home(c echo.Context) error {
 	return c.JSON(http.StatusOK, struct {
@@ -26,54 +13,9 @@ func Home(c echo.Context) error {
 }
 
 func GetPrayerTimes(c echo.Context) error {
-
-	geoData, err := services.GetGeoLocation()
+	prayerTimes, err := prayers.GetPrayersData()
 	if err != nil {
-		return GetUserIPDataError
+		return err
 	}
-
-	methods, err := services.GetMethods()
-	if err != nil {
-		return GetPrayerMethodsError
-	}
-
-	nearestMethodID := utils.GetNearestMethod(&geoData.Coords, methods)
-	prayerTimesParams := &models.PrayerTimesParams{
-		Coords:   geoData.Coords,
-		MethodID: nearestMethodID,
-	}
-
-	prayerTimes, err := services.GetPrayerData(prayerTimesParams)
-	if err != nil {
-		return GetPrayerDataError
-	}
-
 	return c.JSON(http.StatusOK, prayerTimes)
 }
-
-// func GetPrayerTimes(c echo.Context) error {
-// 	clientIP := c.RealIP()
-// 	ipData, err := services.GetIPData(clientIP)
-// 	if err != nil {
-// 		return GetUserIPError
-// 	}
-
-// 	data, err := services.GetMethods()
-// 	if err != nil {
-// 		return GetPrayerMethodsError
-// 	}
-
-// 	nearestMethodID := utils.GetNearestMethod(ipData.Coords, data)
-// 	//fmt.Println(nearestMethodID)
-// 	prayerTimesParams := &models.PrayerTimesParams{
-// 		Coords:   ipData.Coords,
-// 		MethodID: nearestMethodID,
-// 	}
-
-// 	prayerTimes, err := services.GetPrayerData(prayerTimesParams)
-// 	if err != nil {
-// 		return GetPrayerDataError
-// 	}
-
-// 	return c.JSON(http.StatusOK, prayerTimes)
-// }
